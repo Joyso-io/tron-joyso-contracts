b601c286cdf895a6bb0fe9ea855387ee781ab2b9
aker0357 <willy810926@gmail.com>
2018-12-05 15:03:20 +0800

    edit solid version

--- contracts/TokenRegister.sol
+++ contracts/TokenRegister.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.4.24;
+pragma solidity ^0.4.19;
 import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
 
 contract JoysoInterFace {
--- contracts/TokenSender.sol
+++ contracts/TokenSender.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.4.24;
+pragma solidity ^0.4.19;
 import 'zeppelin-solidity/contracts/token/ERC20/ERC20.sol';
 import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
 
