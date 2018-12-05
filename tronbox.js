a24cb0b58c39fbaf8f20ab1caa17995860851a35
Yi-Cyuan Chen <emn178@gmail.com>
2018-12-05 10:20:52 +0800

    upgrade tronbox

--- tronbox.js
+++ tronbox.js
@@ -4,7 +4,7 @@ module.exports = {
   networks: {
     development: {
       consume_user_resource_percent: 30,
-      fee_limit: 100000000,
+      fee_limit: 700000000,
       fullNode: "http://127.0.0.1:8090",
       solidityNode: "http://127.0.0.1:8091",
       eventServer: "http://127.0.0.1:8092",
@@ -13,7 +13,7 @@ module.exports = {
     shasta: {
       privateKey: process.env.DEPLOY_PK,
       consume_user_resource_percent: 30,
-      fee_limit: 100000000,
+      fee_limit: 700000000,
       fullNode: "https://api.shasta.trongrid.io",
       solidityNode: "https://api.shasta.trongrid.io",
       eventServer: "https://api.shasta.trongrid.io",
@@ -22,7 +22,7 @@ module.exports = {
     mainnet: {
       privateKey: process.env.DEPLOY_PK,
       consume_user_resource_percent: 30,
-      fee_limit: 100000000,
+      fee_limit: 700000000,
       fullNode: "https://api.trongrid.io",
       solidityNode: "https://api.trongrid.io",
       eventServer: "https://api.trongrid.io",
