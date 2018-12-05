ce168b4f414600e1b0431a1be50618047420ccf6
Yi-Cyuan Chen <emn178@gmail.com>
2018-12-05 10:56:07 +0800

    enable optimize

--- tronbox.js
+++ tronbox.js
@@ -1,10 +1,16 @@
 require('dotenv').config();
 
 module.exports = {
+  solc: {
+    optimizer: {
+      enabled: true,
+      runs: 200
+    }
+  },
   networks: {
     development: {
       consume_user_resource_percent: 30,
-      fee_limit: 700000000,
+      fee_limit: 300000000,
       fullNode: "http://127.0.0.1:8090",
       solidityNode: "http://127.0.0.1:8091",
       eventServer: "http://127.0.0.1:8092",
@@ -13,7 +19,7 @@ module.exports = {
     shasta: {
       privateKey: process.env.DEPLOY_PK,
       consume_user_resource_percent: 30,
-      fee_limit: 700000000,
+      fee_limit: 300000000,
       fullNode: "https://api.shasta.trongrid.io",
       solidityNode: "https://api.shasta.trongrid.io",
       eventServer: "https://api.shasta.trongrid.io",
@@ -22,7 +28,7 @@ module.exports = {
     mainnet: {
       privateKey: process.env.DEPLOY_PK,
       consume_user_resource_percent: 30,
-      fee_limit: 700000000,
+      fee_limit: 300000000,
       fullNode: "https://api.trongrid.io",
       solidityNode: "https://api.trongrid.io",
       eventServer: "https://api.trongrid.io",
