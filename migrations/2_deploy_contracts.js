5613347ade6f4b5cc0398eabb492e85ad9e4a8b0
Yi-Cyuan Chen <emn178@gmail.com>
2018-12-11 10:58:10 +0800

    update migrate

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,12 @@
 const Joyso = artifacts.require('./Joyso.sol');
+const TokenSender = artifacts.require('./TokenSender.sol');
 const TronWeb = require('tronweb');
 const tronbox = require('../tronbox');
 
+const joy = process.env.JOY_TOKEN;
+
 module.exports = function(deployer) {
-  if (!process.env.JOYSO_WALLET || !process.env.JOY_TOKEN) {
+  if (!process.env.JOYSO_WALLET || !joy) {
     throw new Error('.env is not set.');
   }
   const config = tronbox.networks[deployer.network];
@@ -16,20 +19,37 @@ module.exports = function(deployer) {
       console.log('Block hash: ', block.blockID);
     }
   });
-  deployer.deploy(Joyso, process.env.JOYSO_WALLET, process.env.JOY_TOKEN).then(() => {
-    return Joyso.deployed();
-  }).then(instance => {
-    if (process.env.ADMIN) {
-      const admins = process.env.ADMIN.split(',');
-      admins.forEach(admin => {
-        instance.addToAdmin(admin, true);
-      });
-    }
-    if (process.env.TOKENS) {
-      const tokens = process.env.TOKENS.split(',');
-      tokens.forEach((token, index) => {
-        instance.registerToken(token, index + 2);
-      });
-    }
-  });
+
+  deployer.deploy(Joyso, process.env.JOYSO_WALLET, joy)
+    .then(() => deployer.deploy(TokenSender))
+    .then(() => Promise.all([Joyso.deployed(), TokenSender.deployed()]))
+    .then(([joyso, tokenSender]) => {
+      if (process.env.ADMIN) {
+        const admins = process.env.ADMIN.split(',');
+        admins.forEach(admin => {
+          joyso.addToAdmin(admin, true);
+        });
+      }
+      if (process.env.TOKENS) {
+        const tokens = process.env.TOKENS.split(',');
+        tokens.forEach((token, index) => {
+          joyso.registerToken(token, index + 2);
+        });
+      }
+      if (process.env.TRANSFER_ADMIN) {
+        tokenSender.addToAdmin(process.env.TRANSFER_ADMIN, true);
+      }
+      tokenSender.setJoysoContractAddress(joyso.address);
+      tokenSender.tokenApprove(joy);
+      joyso.addToAdmin(tokenSender.address, true);
+      if (process.env.TRANSFER_ADMIN_PK) {
+        tronWeb.contract().at(joy).then(joyContract => {
+          joyContract.methods.approve(tokenSender.address, '0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff').send({
+            feeLimit: 12000000,
+            callValue: 0,
+            shouldPollResponse: false
+          }, process.env.TRANSFER_ADMIN_PK);
+        });
+      }
+    });
 };
